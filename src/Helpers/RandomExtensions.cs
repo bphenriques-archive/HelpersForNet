59e54c07780a18813a4a7febb16505bc2d0e4a50
Bruno Henriques <brunoaphenriques@gmail.com>
2016-06-27 15:44:00 +0100

    Adds random between two int and between two doubles

---
+++ src/Helpers/RandomExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelpersForNet {
+    public static class RandomExtensions {
+        public static double GetRandomNumber(this Random random, double minimum, double maximum) {
+            return random.NextDouble() * (maximum - minimum) + minimum;
+        }
+
+        public static int GetRandomNumber(this Random random, int minimum, int maximum) {
+            return random.Next(minimum, maximum);
+        }
+    }
+}
