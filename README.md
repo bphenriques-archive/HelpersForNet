f8f6a9d27fb0fc9c22de83ea05301dab5baa5c30
Bruno Henriques <brunoaphenriques@gmail.com>
2016-08-11 17:02:01 +0100

    Adds XMlUtil for serialization

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # HelpersForNet
 Contains several code snippets that I have been using throughout my projects to simplify my life
+
+Thanks to for SerializableDictionary:
+https://weblogs.asp.net/pwelter34/444961
---
+++ src/Helpers/XmlUtil.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using System.Xml.Serialization;
+
+namespace HelpersForNet {
+    /// <summary>
+    /// Xml utilitary for save/load
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class XmlUtil<T> {
+        /// <summary>
+        /// Loads file
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <returns></returns>
+        public static T Load(string filePath) {
+            XmlSerializer serializer = new XmlSerializer(typeof(T));
+            FileStream file = new FileStream(filePath, FileMode.Open);
+            return (T)serializer.Deserialize(file);
+        }
+
+        /// <summary>
+        /// Save file
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <param name="obj"></param>
+        public static void Save(string filePath, T obj) {
+            XmlSerializer serializer = new XmlSerializer(typeof(T));
+            FileStream file = new FileStream(filePath, FileMode.Create);
+            serializer.Serialize(file, obj);
+        }
+    }
+}
