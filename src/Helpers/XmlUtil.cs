70c1624691691deb7b3a1c5aa2c7ad95746466d1
Bruno Henriques <brunoaphenriques@gmail.com>
2016-08-12 11:19:58 +0100

    Removes exception on file not found in save

--- src/Helpers/XmlUtil.cs
+++ src/Helpers/XmlUtil.cs
@@ -34,9 +34,6 @@ namespace HelpersForNet {
         /// <param name="filePath"></param>
         /// <param name="obj"></param>
         public static void Save(string filePath, T obj) {
-            if (!File.Exists(filePath))
-                throw new FileNotFoundException("Couldn't find file at: " + filePath);
-
             XmlSerializer serializer = new XmlSerializer(typeof(T));
             using (FileStream file = new FileStream(filePath, FileMode.Create)) {
                 serializer.Serialize(file, obj);
